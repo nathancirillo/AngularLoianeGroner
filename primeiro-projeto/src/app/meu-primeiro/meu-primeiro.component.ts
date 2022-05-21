fae02bf0e0116b321719b846124b5d5555953f5e
Nathan Cirillo <cirillo.nathan@gmail.com>
2022-05-21 20:39:45 -0300

    primeiro-projeto commit inicial

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+   selector: 'meu-primeiro-componente',
+   template: `
+        <p>Meu primeiro componente em angular!</p>
+   `
+})
+
+export class MeuPrimeiroComponent {}
